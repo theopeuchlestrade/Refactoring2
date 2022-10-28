1b9a3faeb565e72f877399d02d39cb60c97e1a63
Julian63800 <pet.dog63800@gmail.com>
2022-10-28 09:01:50 +0200

    Add comments to the code

--- TheatricalPlayers/src/main/java/StatementPrinter.java
+++ TheatricalPlayers/src/main/java/StatementPrinter.java
@@ -6,16 +6,21 @@ public class StatementPrinter {
   public String print(Invoice invoice, Map<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
-    StringBuilder res = new StringBuilder( "Statement for ");
-    res.append(invoice.customer);
-    res.append("\n");
 
+    //Header of the "string" with the customer name
+    StringBuilder result = new StringBuilder( "Statement for ");
+    result.append(invoice.customer);
+    result.append("\n");
+
+    //Transform the format of numbers from US to European and vice versa
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
       int thisAmount;
 
+      // Use of different strategies depending on the type of the piece
+      // To know the Amount it will need
       switch (play.type) {
         case "tragedy":
           thisAmount = 40000;
@@ -30,33 +35,37 @@ public class StatementPrinter {
           }
           thisAmount += 300 * perf.audience;
           break;
+        // If the type of piece is not define or unknown
+        // then return an error
         default:
           throw new Error("unknown type: ${play.type}");
       }
 
-      // add volume credits
+      // Add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
+      // Add extra credit for every ten comedy attendees
       if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
-      // print line for this order
-      res.append("  ");
-      res.append(play.name);
-      res.append(": ");
-      res.append(frmt.format(thisAmount / 100));
-      res.append(" (");
-      res.append(perf.audience);
-      res.append(" seats)\n");
+      // Print line for this order
+      result.append("  ");
+      result.append(play.name);
+      result.append(": ");
+      result.append(frmt.format(thisAmount / 100));
+      result.append(" (");
+      result.append(perf.audience);
+      result.append(" seats)\n");
       totalAmount += thisAmount;
     }
-    res.append("Amount owed is ");
-    res.append(frmt.format(totalAmount / 100));
-    res.append("\n");
+    // Total amount awed by this customer
+    result.append("Amount owed is ");
+    result.append(frmt.format(totalAmount / 100));
+    result.append("\n");
 
-    res.append("You earned ");
-    res.append(volumeCredits);
-    res.append(" credits\n");
-    return res.toString();
+    // Total credits earned by this customer
+    result.append("You earned ");
+    result.append(volumeCredits);
+    result.append(" credits\n");
+    return result.toString();
   }
 
 }
