a78531d4cfbafed5626d10274d1f05c5d2b2761a
Julian63800 <pet.dog63800@gmail.com>
2022-10-28 09:46:20 +0200

    Using double and float instead of int divided by 100

--- TheatricalPlayers/src/main/java/StatementPrinter.java
+++ TheatricalPlayers/src/main/java/StatementPrinter.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class StatementPrinter {
 
   public String print(Invoice invoice, Map<String, Play> plays) {
-    int totalAmount = 0;
+    double totalAmount = 0;
     int volumeCredits = 0;
 
     //Header of the "string" with the customer name
@@ -13,32 +13,33 @@ public class StatementPrinter {
     res.append("\n");
 
     //Transform the format of numbers from US to European and vice versa
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+    NumberFormat nbFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount;
+      double thisAmount;
 
       // Use of different strategies depending on the type of the piece
       // To know the Amount it will need
       switch (play.type) {
         case "tragedy":
-          thisAmount = 40000;
+          thisAmount = 400.00;
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            thisAmount += 10.00 * (perf.audience - 30);
           }
           break;
         case "comedy":
-          thisAmount = 30000;
+          thisAmount = 300.00;
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            thisAmount += 100.00 + 5.00 * (perf.audience - 20);
           }
-          thisAmount += 300 * perf.audience;
+          thisAmount += 3.00 * perf.audience;
           break;
         // If the type of piece is "Unknown type"
         // then return an error
         case "Unknown type":
           throw new Error("unknown type: ${play.type}");
+        //Never goes in the default
         default:
           throw new Error("WTF is happening ! Why are we here !! Oh no no nooooo");
       }
@@ -52,7 +53,7 @@ public class StatementPrinter {
       res.append("  ");
       res.append(play.name);
       res.append(": ");
-      res.append(frmt.format(thisAmount / 100));
+      res.append(nbFormat.format(thisAmount));
       res.append(" (");
       res.append(perf.audience);
       res.append(" seats)\n");
@@ -60,7 +61,7 @@ public class StatementPrinter {
     }
     // Total amount awed by this customer
     res.append("Amount owed is ");
-    res.append(frmt.format(totalAmount / 100));
+    res.append(nbFormat.format(totalAmount));
     res.append("\n");
 
     // Total credits earned by this customer
